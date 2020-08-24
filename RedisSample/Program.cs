523095b31553cf78e99e9b43ff7ae08c931f10bc
KiranMangu <kiranmohanm@gmail.com>
2020-08-24 22:54:38 +0530

    initial commit

---
+++ RedisSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using StackExchange.Redis;
+namespace RedisSample
+{
+    class Program
+    {
+        private static IDatabase database;
+        public Program()
+        {
+
+        }
+
+        //public ConnectionMultiplexer ConnectRedis
+        //{
+        //    get
+        //    {
+        //        return 
+        //    }
+        //}
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+                ConnectionMultiplexer ConnectRedis = ConnectionMultiplexer.Connect("192.168.0.94");
+                database = ConnectRedis.GetDatabase();
+                var retValue = database.StringGet("first");
+                Console.WriteLine("Redis Retvalue: " + retValue);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
